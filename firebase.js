b3b7d8e0469a6b1098f4ea6776d9057885807146
IB-HOME-REAL-ESTATE <shahzaibabdulsalam93@gmail.com>
2026-07-15 16:44:10 +0500

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,31 +1,19 @@
-import { getFirestore } from "firebase/firestore";
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-apiKey: "AIzaSyDEvn3HcQaxErlqRpwlOZEl7gGOb5ojDOU",
-authDomain: "ib-home-real-estate.firebaseapp.com",
-projectId: "ib-home-real-estate",
-storageBucket: "ib-home-real-estate.firebasestorage.app",
-messagingSenderId: "443981507271",
-appId: "1:443981507271:web:5f2e434d39671c73c7ace2",
-measurementId: "G-HBZ7DZQ2D1"
+  apiKey: "AIzaSyDEvn3HcQaxErlqRpwlOZEl7gGOb5ojDOU",
+  authDomain: "ib-home-real-estate.firebaseapp.com",
+  projectId: "ib-home-real-estate",
+  storageBucket: "ib-home-real-estate.firebasestorage.app",
+  messagingSenderId: "443981507271",
+  appId: "1:443981507271:web:5f2e434d39671c73c7ace2",
+  measurementId: "G-HBZ7DZQ2D1"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-import { getFirestore } from "firebase/firestore";
-
 const db = getFirestore(app);
 
-export { db };
-export { app };
-const db = getFirestore(app);
-
-export { db };
+export { app, db };
